55d1cd1a9cc8819ea3bb5c62af4c24db43f1038f
Maserati <maserati@localhost.localdomain>
2025-03-22 15:52:04 -0400

    Versao com funcao retornando o texto de cada tag

--- main.go
+++ main.go
@@ -5,23 +5,21 @@ import (
 	"io"
 	"net/http"
 	"regexp"
+	"strings"
 )
 
-//42:40
-
-var url string
-
 func main(){
+	var url string
+
 	url = "https://pt.wikipedia.org/wiki/Wikip%C3%A9dia:P%C3%A1gina_principal"
 	response := request(url)
 
-	//fmt.Println(response)
-	fmt.Println()
-	fmt.Println()
-	fmt.Println()
+	tag := "div"
+	blocks := collectAllTagBlocks(response, tag)
 
-	fmt.Println(len(collectTagBlocks(response, "div")))
-
+	for _, block := range blocks {
+		fmt.Println(extractTextTag(block))
+	}
 }
 
 // Faz a requisicao
@@ -42,11 +40,47 @@ func request(url string) string {
 	return fmt.Sprintf("Error? Recived code %d", resp.StatusCode)
 }
 
-// Encontra as tags
-func collectTagBlocks(html, tag string) []string {
+// Extrai todas tags
+func collectAllTagBlocks(html, tag string) []string {
     regexPattern := fmt.Sprintf(`<%[1]s\b[^>]*>(.*?)</%[1]s>`, tag)
     re := regexp.MustCompile(regexPattern)
     matches := re.FindAllString(html, -1)
 
     return matches
 }
+
+// Extrai o texto de cada tag
+func extractTextTag(html string) string {
+    re := regexp.MustCompile(`<[^>]*>`)
+    return strings.TrimSpace(re.ReplaceAllString(html, ""))
+}
+
+
+/*
+func extractTextTagS(html string) []string {
+	tags := []string{"div", "span"}
+	texts := []string{}
+	for {
+		for _, tag := range(tags) {
+			if strings.Contains(html, tag) {
+				fmt.Println(html)
+				regexPattern := fmt.Sprintf(`<%[1]s\b[^>]*>(.*?)</%[1]s>`, tag)
+				re := regexp.MustCompile(regexPattern)
+
+				// Encontra todas as correspondências
+				matches := re.FindAllStringSubmatch(html, -1)
+
+				// Extrair apenas o conteúdo interno da tag
+				for _, match := range matches {
+					if len(match) > 1 {
+						texts = append(texts, match[1]) // match[1] contém o conteúdo interno
+					}
+				}
+			} else {
+				break
+			}
+		}
+	}
+    return texts
+}
+*/
